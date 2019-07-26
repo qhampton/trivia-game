782e40c4cb47d9d6fa58b0df205808dea2430bf0
Quinlan Hampton <qnhampton93@gmail.com>
2019-07-26 11:03:22 -0700

    updated html with correct button of start, updated css with bold titles, commented js for functionality

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+//start button which has on start function
+    //countdown timer runs on click
+    //displays questions from list below
+        //object array of 8 questions *only one answer per question
+    //submit button displays on the bottom
+
+//when submit button is clicked
+    //function to check question answers
+    //post score
+        //have a switch depending on scores
