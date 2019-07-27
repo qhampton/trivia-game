152faf808e6ad059a4b9e6a86de1d6c790eff468
Quinlan Hampton <qnhampton93@gmail.com>
2019-07-26 22:28:18 -0700

    fixed timer section and added a submit button section in html, added timer variables and functions to count down from 5 minutes, also a submit button, both display on click of the start button in js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,79 @@
 //start page
 $( document ).ready(function() {
+//list of questions in array of objects
+var quizQuestions = [
+    {question:"What is Harry Potter's first pet?",
+    choices:["cat", "toad", "owl", "rat"],
+    answer:"Owl"
+    },
+    {question:"There are 7 Weasley siblings.",
+    choices:["True", "False"],
+    answer:"True"
+    },
+    {question:"How can a house elf be freed?",
+    choices:["Nothing", "Clothing from their Master", "After a certain age", "A gift from their Master"],
+    answer:"Clothing from their Master"
+    },
+    {question:"What are Hermione's parents occuption?",
+    choices:["Dentists", "Teachers","Doctors", "Solisitors"],
+    answer:"Dentists"
+    },
+    {question:"What position does Ron play in quidditch?",
+    choices:["Seeker", "Beater", "Chaser", "Keeper"],
+    answer:"Keeper"
+    },
+    {question:"Harry and Voldemort are related.",
+    choices:["True", "False"],
+    answer:"True"
+    },
+    {question:"Who does Harry Potter meet first from the Wizarding World (not including his parents)?",
+    choices:["Ron", "Hermione", "Neville", "Malfoy"],
+    answer:"Malfoy"
+    },
+    {question:"When did the battle of Hogwarts take place?",
+    choices:["1850", "2010","1760","1998"],
+    answer:"1998",
+    }];
+console.log(quizQuestions);
+//time variables- 5 minutes
+var number = 300;
+var gameTime;
+//time variables to call within button clicks
+//counts down in seconds from 300 and displas the time
+function countDown(){
+    number--;
+    $("#time-left").html("<h3> You have " + number + " seconds left!</h>");
+    if (number === 0) {
+    stop();}
+}
+//pulls the countDown function and rate  of decrease together
+function start() {
+    gameTime = setInterval(countDown, 1000);
+}
+//what happens when time runs out
+function stop(){
+    clearInterval(gameTime);
+}
+
 //start button which has on start function
+$("#startGame").click(function(){
+    //hide start button
+    $("#startGame").hide();
     //countdown timer runs on click
-    //displays questions from list below
-
+    start();
+    //displays questions from list above
+    // $.each(quizQuestions, function(question, choice) {
+    // $("#questions-here").prepend(quizQuestions)});
         //object array of 8 questions *only one answer per question
-        var quizQuestions = [
-            {question:"What is Harry Potter's first pet?",
-            choices:["cat", "toad", "owl", "rat"],
-            answer:"Owl"
-            },
-            {question:"There are 7 Weasley siblings.",
-            choices:["True", "False"],
-            answer:"True"
-            },
-            {question:"How can a house elf be freed?",
-            choices:["Nothing", "Clothing from their Master", "After a certain age", "A gift from their Master"],
-            answer:"Clothing from their Master"
-            },
-            {question:"What are Hermione's parents occuption?",
-            choices:["Dentists", "Teachers","Doctors", "Solisitors"],
-            answer:"Dentists"
-            },
-            {question:"What position does Ron play in quidditch?",
-            choices:["Seeker", "Beater", "Chaser", "Keeper"],
-            answer:"Keeper"
-            },
-            {question:"Harry and Voldemort are related.",
-            choices:["True", "False"],
-            answer:"True"
-            },
-            {question:"Who does Harry Potter meet first from the Wizarding World (not including his parents)?",
-            choices:["Ron", "Hermione", "Neville", "Malfoy"],
-            answer:"Malfoy"
-            },
-            {question:"When did the battle of Hogwarts take place?",
-            choices:["1850", "2010","1760","1998"],
-            answer:"1998",
-            }];
-    console.log(quizQuestions);
     //submit button displays on the bottom
-
+    var quizEnd = $("<button/>",
+    {
+        class: "btn btn-primary btn-lg btn-block",
+        text: "Finite!",
+        click: function () {}
+    });
+    $("#submit").prepend(quizEnd);
+});
 //when submit button is clicked
     //function to check question answers
     //post score
