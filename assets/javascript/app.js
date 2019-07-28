0152fa7309354d92145c97bb02ee24e6a395751b
Quinlan Hampton <qnhampton93@gmail.com>
2019-07-27 22:28:46 -0700

    fixed js files to be able to run through questions and added finished button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,90 +1,127 @@
 //start page
 $(document).ready(function () {
-    //questionstheir own seperate file- too long to put one one 
+    //questions on their own separate file- too long to put one 
+    //submit button
+    $(document).on("click","#finished", function(){
+    game.done();
+    });
     //----------------------------Timer -----------------------------
     //time variables- 5 minutes (300 seconds)
     var number = 15;
     var gameTime;
 
-    //counts down in seconds from 300 and displas the time
+    //counts down in seconds from 300 and displays the time
     function countDown() {
         number--;
         $("#time-left").html("<h3> You have " + number + " seconds left!</h>");
         if (number === 0) {
-            stop();
-        }
-    }
+        stop();
+        }}
 
     //pulls the countDown function and rate  of decrease together
     function start() {
         gameTime = setInterval(countDown, 1000);
     }
-    
+
     //what happens when time runs out
     function stop() {
-        clearInterval(gameTime);
-        console.log("Times Up!");
+        clearInterval(gameTime)
         game.result;
+        console.log("Times Up");
     }
-    //-----Game variables and Functions -----------------
-    //how to check for answers -using a variable with methonds within int to check each question to th answered checked and then showing the total
-    var game = {
-        correct: 0,
-        incorrect: 0, 
+//-----Game variables and Functions -----------------
+//how to check for answers -using a variable with methods within int to check each question to the answered checked and then showing the total
+var game = {
+    correct: 0,
+    incorrect: 0,
 
-        //calculates the correct and incorrect answers 
-        done: function(){
-            $.each($("input [name= 'question-1':checked")),
-            function(){
-                if($(this).val()==question[0].answer){
-                    game.correct++;
-                }
-                else{
-                    game.incorrect++;
-                }
-            this.result();
-            }},
-
-        //prints score to page
-        result: function(){
-            clearInterval(gameTime)
-            $("time-left").html("Donee")
-            $("#questions-here").append("<h3> Correct answers: " + this.correcet);
-            $("#questions-here").append("<h3> Correct answers: " + this.incorrecet);
-            // if(this.correct > 6){
-            //     $("#mainPic").attr("<img>", assets/images/you-win.gif);
-            // }
-            // else{
-            //     $("#mainPic").attr("<img>", assets/images/you-lose.gif);
-            // }
-        },
-    }
-
-    //---------------------Once Start Button Is Clicked-----------------------------
-    //once button is clicked it will hide and hide the main photo, bring questions and submit button up
-    $("#startGame").click(function () {
-        //hide start button
-        $("#startGame").hide();
-        $("#mainPic").hide();
-
-        //countdown timer runs on click
-        start();
-
-        //displays questions from other file
-        for (var i = 0; i < questions.length; i++) {
-            $("#questions-here").append("<h3>" + questions[i].question + "</h3>");
-            for (var j = 0; j < questions[i].choices.length; j++) {
-                $("#questions-here").append("<input type='radio' name='question- " + i + "' value='" + questions[i].choices[j] + "'>" + questions[i].choices[j] + "<br>");
+    //calculates the correct and incorrect answers 
+    done:function () {
+        $.each($("input [name= 'question-0':checked"), function () {
+            if ($(this).val() == question[0].answer) {
+                game.correct++;
+            } 
+            else {
+                game.incorrect++;
             }
-            $("#questions-here").append("<br>");
-        }
-        //submit button displays on the bottom
-        var quizEnd = $("<button/>",
-        {
-            class: "btn btn-primary btn-lg btn-block",
-            text: "Finite!",
-            click: function () {}
         });
-        $("#submit").prepend(quizEnd);
-    });
+        $.each($("input [name= 'question-1':checked"), function () {
+            if ($(this).val() == question[1].answer) {
+                game.correct++;
+            } 
+            else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input [name= 'question-2':checked"), function () {
+            if ($(this).val() == question[2].answer) {
+            game.correct++;
+            } 
+            else {
+            game.incorrect++;
+            }
+        }); 
+        $.each($("input [name= 'question-3':checked"), function () {
+            if ($(this).val() == question[3].answer) {
+                game.correct++;
+            } 
+            else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input [name= 'question-4':checked"), function () {
+            if ($(this).val() == question[4].answer) {
+                game.correct++;
+            } 
+            else {
+                game.incorrect++;
+            }
+        });
+        $.each($("input [name= 'question-5':checked"), function () {
+            if ($(this).val() == question[5].answer) {
+                game.correct++;
+            } 
+            else {
+                game.incorrect++;
+            }
+        });
+        console.log(game.incorrect + " incorrect");
+        console.log(game.correct + " correct");
+        game.result();
+    },
+    //prints score to page
+    result:function(){
+        $("#time-left").remove();
+        $("#time-left").html("<h2>Times Up!</h2>");
+        $("#questions-here").append("<h3> Correct answers: " + this.correct);
+        $("#questions-here").append("<h3> Correct answers: " + this.incorrect);
+        // if(this.correct > 6){
+        //     $("#mainPic").attr("<img>", assets/images/you-win.gif);
+        // }
+        // else{
+        //     $("#mainPic").attr("<img>", assets/images/you-lose.gif);
+        // }
+},
+};
+
+//---------------------Once Start Button Is Clicked-----------------------------
+//once button is clicked it will hide and hide the main photo, bring questions and submit button up
+$("#startGame").click(function () {
+    //hide start button
+    $("#startGame").hide();
+    $("#mainPic").hide();
+
+    //countdown timer runs on click
+    start();
+
+    //displays questions from other file
+    for (var i = 0; i < questions.length; i++) {
+        $("#questions-here").append("<h3>" + questions[i].question + "</h3>");
+        for (var j = 0; j < questions[i].choices.length; j++) {
+            $("#questions-here").append("<input type='radio' name='question- " + i + "' value='" + questions[i].choices[j] + "'>" + questions[i].choices[j] + "<br>");
+        }
+        $("#questions-here").append("<br>");
+        }
+    $("#questions-here").append("<button type='button' class='btn btn-primary btn-lg btn-block'  id='finished'>Finite!</button>");
+});
 });
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,33 +1,33 @@
     //-----------list of questions in array of objects
     var questions = [{
-        question: "What is Harry Potter's first pet?",
-        choices: ["cat", "toad", "owl", "rat"],
-        answer: "Owl"
-    },
-    {
-        question: "How can a house elf be freed?",
-        choices: ["Nothing", "Clothing from their Master", "After a certain age", "A gift from their Master"],
-        answer: "Clothing from their Master"
-    },
-    {
-        question: "What position does Ron play in quidditch?",
-        choices: ["Seeker", "Beater", "Chaser", "Keeper"],
-        answer: "Keeper"
-    },
-    {
-        question: "Harry and Voldemort are related.",
-        choices: ["True", "False"],
-        answer: "True"
-    },
-    {
-        question: "Who does Harry Potter meet first from the Wizarding World (not including his parents)?",
-        choices: ["Ron", "Hermione", "Neville", "Malfoy"],
-        answer: "Malfoy"
-    },
-    {
-        question: "When did the battle of Hogwarts take place?",
-        choices: ["1850", "2010", "1760", "1998"],
-        answer: "1998",
-    }
-];
-console.log(questions);
+            question: "What is Harry Potter's first pet?",
+            choices: ["cat", "toad", "owl", "rat"],
+            answer: "Owl"
+        },
+        {
+            question: "How can a house elf be freed?",
+            choices: ["Nothing", "Clothing from their Master", "After a certain age", "A gift from their Master"],
+            answer: "Clothing from their Master"
+        },
+        {
+            question: "What position does Ron play in quidditch?",
+            choices: ["Seeker", "Beater", "Chaser", "Keeper"],
+            answer: "Keeper"
+        },
+        {
+            question: "Harry and Voldemort are related.",
+            choices: ["True", "False"],
+            answer: "True"
+        },
+        {
+            question: "Who does Harry Potter meet first from the Wizarding World (not including his parents)?",
+            choices: ["Ron", "Hermione", "Neville", "Malfoy"],
+            answer: "Malfoy"
+        },
+        {
+            question: "When did the battle of Hogwarts take place?",
+            choices: ["1850", "2010", "1760", "1998"],
+            answer: "1998",
+        }
+    ];
+    console.log(questions);
